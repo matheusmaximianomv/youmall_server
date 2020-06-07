47aeacf54caa514f92b6782329ba554a4bb74608
matheusmaximianomv <matheusmaximianomv@aluno.fapce.edu.br>
2020-06-07 12:29:55 -0300

    Structure: Criação de um novo usuário

--- src/database/seeds/generate_users.js
+++ src/database/seeds/generate_users.js
@@ -10,5 +10,13 @@ exports.seed = async (knex) => {
       cpf: '123.456.789-10',
       isAdmin: true,
     },
+    {
+      fullname: 'Lucas Serafim de Sousa',
+      username: 'Lucas S.',
+      email: 'lucas@email.com',
+      password: await Bcrypt.hash('123456789', 12),
+      cpf: '109.876.543-21',
+      isAdmin: true,
+    },
   ]);
 };
